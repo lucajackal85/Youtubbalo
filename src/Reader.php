7ad8174e514779bba39017b222f2b001cabd8ecf
Luca <luca.giacalone@edimotive.com>
2018-06-06 09:23:51 +0200

    Aggiunto cache reader

--- src/Reader.php
+++ src/Reader.php
@@ -2,6 +2,8 @@
 
 namespace Jackal\Youtubbalo;
 
+use Symfony\Component\Cache\Simple\FilesystemCache;
+
 class Reader extends BaseYoutubeApi
 {
 
@@ -10,31 +12,41 @@ class Reader extends BaseYoutubeApi
      * @param int $maxResult
      * @return array
      */
-    public function getVideoFromPlaylist($playlistId,$maxResult = 25){
+    public function getVideoFromPlaylist($playlistId,$maxResult = 25,$cacheTTL = 3600){
 
-        $service = $this->getService();
+        $cacheKey = 'youtubbalo.playlist_'.$playlistId;
 
-        $response = $service->playlistItems->listPlaylistItems('snippet,contentDetails',[
+        $cache = new FilesystemCache();
+
+        if(!$cache->has($cacheKey)) {
+            $service = $this->getService();
+
+            $response = $service->playlistItems->listPlaylistItems('snippet,contentDetails', [
                 'maxResults' => $maxResult,
                 'playlistId' => $playlistId
-        ]);
+            ]);
 
-        $out = [];
-        foreach($response->getItems() as $item){
-            /** @var \Google_Service_YouTube_PlaylistItemSnippet $snippet */
-            $snippet = $item->getSnippet();
+            $out = [];
+            foreach ($response->getItems() as $item) {
+                /** @var \Google_Service_YouTube_PlaylistItemSnippet $snippet */
+                $snippet = $item->getSnippet();
 
-            $out[] = [
-                'id' => $snippet->getResourceId()->getVideoId(),
-                'title' => $snippet->getTitle(),
-                'description' => $snippet->getDescription(),
-                'published_at' => new \DateTime($snippet->getPublishedAt()),
-                'playlist_id' => $snippet->getPlaylistId(),
-                'channel_id' => $snippet->getChannelId(),
-                'channel_title' => $snippet->getChannelTitle()
-            ];
+                $out[] = [
+                    'id' => $snippet->getResourceId()->getVideoId(),
+                    'title' => $snippet->getTitle(),
+                    'description' => $snippet->getDescription(),
+                    'published_at' => new \DateTime($snippet->getPublishedAt()),
+                    'playlist_id' => $snippet->getPlaylistId(),
+                    'channel_id' => $snippet->getChannelId(),
+                    'channel_title' => $snippet->getChannelTitle()
+                ];
+            }
+
+            $cache->set($cacheKey,$out,$cacheTTL);
         }
 
-        return $out;
+
+
+        return $cache->get($cacheKey);
     }
 }
